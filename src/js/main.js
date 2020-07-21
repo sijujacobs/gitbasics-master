73ae5aca58f6674a5d3262f6e4aad335aa3fc299
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-07-21 13:44:29 -0400

    initial commit to MASTER

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+console.log("-------MAIN.JS-----------");
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git remote add origin https://github.com/sijujacobs/gitbasics-master.git
+// git push -u origin master
