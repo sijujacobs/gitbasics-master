39161baf32ee946c626d0cee8a8d4741fd83ca51
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-07-21 22:39:46 -0400

    commit helloworld

--- src/js/main.js
+++ src/js/main.js
@@ -21,4 +21,11 @@ function hello(a, b, c, d) {
   console.log("-------siju 9:44PM EST");
   return Number(a) + Number(b) + Number(c) + Number(d);
 }
+
+function helloWorld() {
+  console.log("let me test");
+  console.log("-------siju 9:22PM EST");
+  console.log("let me test--new");
+  console.log("-------siju 9:44PM EST");
+}
 add(5, 4, 8, 7);
