b48b7c487ad6d7ed4a1cdf41ff6efb208d899d3f
punith kumar kakkera <punith@puniths-MacBook-Pro.local>
2020-07-21 21:33:10 -0500

    hello v3

--- src/js/main.js
+++ src/js/main.js
@@ -14,5 +14,11 @@ function add(a, b, c, d) {
   console.log("-------siju 9:44PM EST");
   return Number(a) + Number(b) + Number(c) + Number(d);
 }
-
+function hello(a, b, c, d) {
+  console.log("let me test");
+  console.log("-------siju 9:22PM EST");
+  console.log("let me test--new");
+  console.log("-------siju 9:44PM EST");
+  return Number(a) + Number(b) + Number(c) + Number(d);
+}
 add(5, 4, 8, 7);
